c1f2dcf915c8871e0ca51ece354d1b2a88684e76
Larissa Ferreira <164913651+Larissarff@users.noreply.github.com>
2024-09-03 12:53:14 -0300

    Create Plus.cs

---
+++ ConverteParabinario.cs/Plus.cs
@@ -0,0 +1,6 @@
+// Solicita os números ao usuário
+        Console.WriteLine("Digite o primeiro número:");
+        numero.Num1 = int.Parse(Console.ReadLine()); // Lê e converte a entrada para int
+
+        Console.WriteLine("Digite o segundo número:");
+        numero.Num2 = int.Parse(Console.ReadLine()); // Lê e converte a entrada para int
